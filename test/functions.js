fa861c552dc4eca50247d3b964253aa0e591e8ec
hongmaoxiao <769501294@qq.com>
2017-10-01 23:08:26 +0800

    version 0.2.0 is out, with inject -> reduce, JS standard methodname aliases, a compose(), and a lastIndexOf()

--- test/functions.js
+++ test/functions.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-
+  
   module("Function functions (bind, bindAll, and so on...)");
-
+    
   test("functions: bind", function() {
     var context = {name : 'moe'};
     var func = function() { return "name: " + this.name; };
     var bound = _.bind(func, context);
     equals(bound(), 'name: moe', 'can bind a function to a context');
-
+    
     var func = function(salutation, name) { return salutation + ': ' + name; };
     func = _.bind(func, this, 'hello');
     equals(func('moe'), 'hello: moe', 'the function was partially applied in advance');
-
+    
     func = _.bind(func, this, 'curly');
     equals(func(), 'hello: curly', 'the function was completely applied in advance');
   });
-
+  
   test("functions: bindAll", function() {
     var curly = {name : 'curly'}, moe = {
       name    : 'moe',
@@ -28,24 +28,34 @@ $(document).ready(function() {
     equals(curly.getName(), 'name: curly', 'unbound function is bound to current object');
     equals(curly.sayHi(), 'hi: moe', 'bound function is still bound to original object');
   });
-
+  
   asyncTest("functions: delay", function() {
     var delayed = false;
     _.delay(function(){ delayed = true; }, 100);
     _.delay(function(){ ok(!delayed, "didn't delay the function quite yet"); }, 50);
     _.delay(function(){ ok(delayed, 'delayed the function'); start(); }, 150);
   });
-
+  
   asyncTest("functions: defer", function() {
     var deferred = false;
     _.defer(function(bool){ deferred = bool; }, true);
     _.delay(function(){ ok(deferred, "deferred the function"); start(); }, 50);
   });
-
+  
   test("functions: wrap", function() {
     var greet = function(name){ return "hi: " + name; };
     var backwards = _.wrap(greet, function(func, name){ return func(name) + ' ' + name.split('').reverse().join(''); });
     equals(backwards('moe'), 'hi: moe eom', 'wrapped the saluation function');
   });
-
+  
+  test("functions: compose", function() {
+    var greet = function(name){ return "hi: " + name; };
+    var exclaim = function(sentence){ return sentence + '!'; };
+    var composed = _.compose(exclaim, greet);
+    equals(composed('moe'), 'hi: moe!', 'can compose a function that takes another');
+    
+    composed = _.compose(greet, exclaim);
+    equals(composed('moe'), 'hi: moe!', 'in this case, the functions are also commutative');
+  });
+  
 });
--- test/utility.js
+++ test/utility.js
@@ -1,24 +1,28 @@
 $(document).ready(function() {
+  
   module("Utility functions (uniqueId, template)");
-
+  
   test("utility: noConflict", function() {
     var underscore = _.noConflict();
     ok(underscore.isUndefined(_), "The '_' variable has been returned to its previous state.");
+    var intersection = underscore.intersect([-1, 0, 1, 2], [1, 2, 3, 4]);
+    equals(intersection.join(', '), '1, 2', 'but the intersection function still works');
     window._ = underscore;
   });
-
-  test("utility: uniqueId", function(){
-    var ids = []; i = 0;
+    
+  test("utility: uniqueId", function() {
+    var ids = [], i = 0;
     while(i++ < 100) ids.push(_.uniqueId());
-    equals(_.uniq(ids).length, ids.length, 'can generate a global-unique stream of ids');
+    equals(_.uniq(ids).length, ids.length, 'can generate a globally-unique stream of ids');
   });
-
-  test("utility: template", function(){
+  
+  test("utility: template", function() {
     var basicTemplate = _.template("<%= thing %> is gettin' on my noives!");
-    var result = basicTemplate({thing: 'This'});
+    var result = basicTemplate({thing : 'This'});
     equals(result, "This is gettin' on my noives!", 'can do basic attribute interpolation');
     var fancyTemplate = _.template("<% for (key in people) { %><li><%= people[key] %></li><% } %>");
-    result = fancyTemplate({people: { moe: "Moe", larry: "Larry", curly: "Curly"}});
+    result = fancyTemplate({people : {moe : "Moe", larry : "Larry", curly : "Curly"}});
     equals(result, "<li>Moe</li><li>Larry</li><li>Curly</li>", 'can run arbitrary javascript in templates');
   });
+  
 });
