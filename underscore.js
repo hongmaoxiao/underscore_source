d97e9c8c8d400ea022a3fdf5e338ca1db40ac13c
hongmaoxiao <769501294@qq.com>
2017-10-02 21:41:53 +0800

    adding a reduceRight (it's in JS 1.8), and aliasing foldl and foldr

--- underscore.js
+++ underscore.js
@@ -69,14 +69,28 @@
     };
 
     // Reduce builds up a single result from a list of values. Also known as
-    // inject, or foldl.
+    // inject, or foldl. Uses JavaScript 1.8's version of reduce, if possible.
     _.reduce = function(obj, memo, iterator, context) {
+        if (obj && obj.reduce) {
+            return obj.reduce(_.bind(iterator, context), memo);
+        }
         _.each(obj, function(value, index, list) {
             memo = iterator.call(context, memo, value, index, list);
         });
         return memo;
     };
 
+    // The right-associative version of reduce, also known as foldr. Uses
+    // JavaScript 1.8's version of reduceRight, if available.
+    _.reduceRight = function(obj, memo, iterator, context) {
+        if (obj && obj.reduceRight) {
+            return obj.reduceRight(_.bind(iterator, context), memo);
+        }
+        var reversed = _.clone(_.toArray(obj)).reverse();
+        _.each(reversed, function(value, index) {
+            memo = iterator.call(context, memo, value, index, obj);
+        });
+    };
     // Return the first value which passes a truth test.
     _.detect = function(obj, iterator, context) {
         var result;
@@ -425,6 +439,9 @@
 
     // Create a (shallow-cloned) duplicate of an object.
     _.clone = function(obj) {
+        if (_.isArray(obj)) {
+            return obj.slice(0);
+        }
         return _.extend({}, obj);
     };
 
@@ -532,7 +549,8 @@
     /*------------------------------- Aliases ----------------------------------*/
 
     _.forEach = _.each;
-    _.inject = _.reduce;
+    _.foldl = _.inject = _.reduce;
+    _.foldr = _.reduceRight;
     _.filter = _.select;
     _.every = _.all;
     _.some = _.any;
