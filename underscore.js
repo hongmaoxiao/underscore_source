fd605c397e74a75330989bb663e0abec0a00ee9b
hongmaoxiao <769501294@qq.com>
2017-10-04 00:13:11 +0800

    0.4.6 is on the books, with kylichuku's range function

--- underscore.js
+++ underscore.js
@@ -36,37 +36,7 @@
     }
 
     // Current version.
-    _.VERSION = '0.4.5';
-
-  /*------------------------ Generator Functions: ----------------------------*/
-
-  // Generates an Array, containing an arithmetic progressions
-  // Analog of python's built-in function 'range'
-  _.range = function(start, stop, step) {
-    if (!stop) {
-      var stop = start;
-      start = 0;
-    }
-
-    if (!step) {
-      var step = 1;
-    }
-
-    var length = Math.ceil((stop - start) / step);
-
-    if (length < 0) {
-      return [];
-    }
-
-    var results = new Array(length);
-    var resIdx = 0;
-
-    for (var i = start; (start <= stop ? stop - i > 0 : i - stop > 0); i += step) {
-        results[resIdx++] = i;
-    }
-
-    return results;
-  };
+    _.VERSION = '0.4.6';
 
     /*------------------------ Collection Functions: ---------------------------*/
 
@@ -280,7 +250,7 @@
     _.toArray = function(iterable) {
         if (!iterable) return [];
         if (iterable.toArray) {
-          return iterable.toArray();
+            return iterable.toArray();
         }
         if (_.isArray(iterable)) return iterable;
         return _.map(iterable, function(val) {
@@ -399,6 +369,36 @@
             }
         }
         return -1;
+    };
+
+    // Generate an integer Array containing an arithmetic progression. A port of
+    // the native Python range() function. See:
+    // http://docs.python.org/library/functions.html#range
+    _.range = function(start, stop, step) {
+        if (!stop) {
+            var stop = start;
+            start = 0;
+        }
+
+        if (!step) {
+            var step = 1;
+        }
+
+        var length = Math.ceil((stop - start) / step);
+
+        if (length < 0) {
+            return [];
+        }
+
+        var results = new Array(length);
+        var resIdx = 0;
+
+        for (var i = start;
+            (start <= stop ? stop - i > 0 : i - stop > 0); i += step) {
+            results[resIdx++] = i;
+        }
+
+        return results;
     };
 
     /* ----------------------- Function Functions: -----------------------------*/
