d03de8f894b9e47bc73860abf74463e7a688b248
hongmaoxiao <769501294@qq.com>
2017-10-04 23:01:37 +0800

    safer isArguments function -- make sure we're not dealing with strings or functions

--- underscore.js
+++ underscore.js
@@ -608,7 +608,7 @@
 
     // Is a given variable an arguments object?
     _.isArguments = function(obj) {
-        return obj && obj.callee && _.isNumber(obj.length) && !_.isArray(obj) && !propertyIsEnumerable.call(obj, 'length');
+        return obj && _.isNumber(obj.length) && !obj.concat && !obj.substr && !obj.apply && !_.isArray(obj) && !propertyIsEnumerable.call(obj, 'length');
     };
 
     // Is a given value a function?
