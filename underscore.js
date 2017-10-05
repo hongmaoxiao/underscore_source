78fe7e8274e5268bfa669131238367a299291a90
hongmaoxiao <769501294@qq.com>
2017-10-05 16:42:16 +0800

    delegate to ECMA5 native Array.isArray if available

--- underscore.js
+++ underscore.js
@@ -502,7 +502,7 @@
   // ------------------------- Object Functions: ------------------------------
 
   // Retrieve the names of an object's properties.
-  // ECMA5 has Object.keys(obj) in webkit nightlies
+  // Delegates to ECMA5's native Object.keys
   _.keys = Object.keys || function(obj) {
     if (_.isArray(obj)) {
       return _.range(0, obj.length);
@@ -659,7 +659,8 @@
   };
 
   // Is a given value an array?
-  _.isArray = function(obj) {
+  // Delegates to ECMA5's native Array.isArray
+  _.isArray = Array.isArray || function(obj) {
     return !!(obj && obj.concat && obj.unshift);
   };
 
