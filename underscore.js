ca26fafe6c8a58577ace1986c57241031cebf4d1
hongmaoxiao <769501294@qq.com>
2017-10-07 12:04:22 +0800

    Adding header + version number to -min.js

--- underscore.js
+++ underscore.js
@@ -1,3 +1,4 @@
+//     Underscore.js 1.1.2
 //     (c) 2010 Jeremy Ashkenas, DocumentCloud Inc.
 //     Underscore is freely distributable under the MIT license.
 //     Portions of Underscore are inspired or borrowed from Prototype,
@@ -29,19 +30,19 @@
     toString = ObjProto.toString,
     hasOwnProperty = ObjProto.hasOwnProperty;
 
-    // All **ECMAScript 5** native function implementations that we hope to use
-    // are declared here.
-    var nativeForEach = ArrayProto.forEach,
-      nativeMap = ArrayProto.map,
-      nativeReduce = ArrayProto.reduce,
-      nativeReduceRight = ArrayProto.reduceRight,
-      nativeFilter = ArrayProto.filter,
-      nativeEvery = ArrayProto.every,
-      nativeSome = ArrayProto.some,
-      nativeIndexOf = ArrayProto.indexOf,
-      nativeLastIndexOf = ArrayProto.lastIndexOf,
-      nativeIsArray = Array.isArray,
-      nativeKeys = Object.keys;
+  // All **ECMAScript 5** native function implementations that we hope to use
+  // are declared here.
+  var nativeForEach = ArrayProto.forEach,
+    nativeMap = ArrayProto.map,
+    nativeReduce = ArrayProto.reduce,
+    nativeReduceRight = ArrayProto.reduceRight,
+    nativeFilter = ArrayProto.filter,
+    nativeEvery = ArrayProto.every,
+    nativeSome = ArrayProto.some,
+    nativeIndexOf = ArrayProto.indexOf,
+    nativeLastIndexOf = ArrayProto.lastIndexOf,
+    nativeIsArray = Array.isArray,
+    nativeKeys = Object.keys;
 
   // Create a safe reference to the Underscore object for reference below.
   var _ = function(obj) {
