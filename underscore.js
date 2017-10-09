8a3eb01023e05de07367a35a92817b82b296a281
hongmaoxiao <769501294@qq.com>
2017-10-09 22:37:59 +0800

    Adding _.once, for running a function once

--- underscore.js
+++ underscore.js
@@ -567,6 +567,17 @@
     return limit(func, wait, true);
   };
 
+  // Runs a function only if it's never been run before.
+  _.once = function(func) {
+    if (_.indexOf(onceRunFunctions, func)) {
+      onceRunFunctions.push(func);
+      return func.apply(func, slice.call(arguments, 1));
+    }
+  };
+
+  // Internal record of functions that have been run via `_.once`.
+  var onceRunFunctions = [];
+
   // Returns the first function passed as an argument to the second,
   // allowing you to adjust arguments, run code before and after, and
   // conditionally execute the original function.
