b4626477c76b3051f1d009ac181866a2b3da3a49
hongmaoxiao <769501294@qq.com>
2017-10-10 23:07:09 +0800

    stricter ECMA5 compliance. _.every now requires an iterator to be passed. #160

--- underscore.js
+++ underscore.js
@@ -193,7 +193,6 @@
   // Delegates to **ECMAScript 5**'s native `every` if available.
   // Aliased as `all`.
   _.every = _.all = function(obj, iterator, context) {
-    iterator || (iterator = _.identity);
     var result = true;
     if (obj == null) {
       return results;
