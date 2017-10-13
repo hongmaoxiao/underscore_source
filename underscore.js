0645557e24bee27f1bd64cc1f85b4bad33729e3c
Xiaomao <fxm@autoforce.net>
2017-10-13 14:18:43 +0800

    Edited underscore.js via GitHub

--- underscore.js
+++ underscore.js
@@ -462,9 +462,10 @@
     if (nativeIndexOf && array.indexOf === nativeIndexOf) {
       return array.indexOf(item);
     }
-    for (i = 0, l = array.length; i < l; i++) {
-      if (array[i] === item) {
-        return i;
+    var o, i = 0;
+    while(o = array[i++]){
+      if (o === item) {
+        return i - 1;
       }
     }
     return -1;
