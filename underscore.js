3ceeeb5f69d6513ba96ca1fa983b53f049c58d7b
hongmaoxiao <769501294@qq.com>
2017-10-21 01:56:39 +0800

    revisions to #534, sortBy(undefined)

--- underscore.js
+++ underscore.js
@@ -333,10 +333,10 @@
     }).sort(function(left, right) {
       var a = left.criteria,
         b = right.criteria;
-      if (_.isUndefined(a)) {
+      if (a === void 0) {
         return 1;
       }
-      if (_.isUndefined(b)) {
+      if (b === void 0) {
         return -1;
       }
       return a < b ? -1 : a > b ? 1 : 0;
