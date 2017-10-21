b9d0631e19cf0b70a8cee2e5d9426f843a7cba41
hongmaoxiao <769501294@qq.com>
2017-10-21 15:57:14 +0800

    Make immediate-debounce reentrant.

--- underscore.js
+++ underscore.js
@@ -685,11 +685,12 @@
           func.apply(context, args);
         }
       };
-      if (immediate && !timeout) {
-        func.apply(context, args);
-      }
+      var callNow = immediate && !timeout;
       clearTimeout(timeout);
       timeout = setTimeout(later, wait);
+      if (callNow) {
+        func.apply(context, args);
+      }
     };
   };
 
