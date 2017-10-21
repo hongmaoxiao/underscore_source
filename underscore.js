06553fb3276d1637aef54774a8c2def16e86eb5b
hongmaoxiao <769501294@qq.com>
2017-10-22 00:47:58 +0800

    Tweak _.toArray.

--- underscore.js
+++ underscore.js
@@ -393,11 +393,8 @@
   // Safely convert anything iterable into a real, live array.
   _.toArray = function(obj) {
     if (!obj) return [];
-    if (_.isArray(obj)) return slice.call(obj);
-    if (_.isArguments(obj)) {
-      return slice.call(obj);
-    }
-    if (obj.toArray && _.isFunction(obj.toArray)) {
+    if (_.isArray(obj) || _.isArguments(obj)) return slice.call(obj);
+    if (_.isFunction(obj.toArray)) {
       return obj.toArray();
     }
     return _.values(obj);
