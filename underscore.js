c75e109b2741addbc6276de506b92126482e60bd
hongmaoxiao <769501294@qq.com>
2017-10-21 10:45:35 +0800

    Remove the unnecessary branch in _.shuffle()

--- underscore.js
+++ underscore.js
@@ -312,13 +312,9 @@
     var shuffled = [],
       rand;
     each(obj, function(value, index, list) {
-      if (index == 0) {
-        shuffled[0] = value;
-      } else {
-        rand = Math.floor(Math.random() * (index + 1));
-        shuffled[index] = shuffled[rand];
-        shuffled[rand] = value;
-      }
+      rand = Math.floor(Math.random() * (index + 1));
+      shuffled[index] = shuffled[rand];
+      shuffled[rand] = value;
     });
     return shuffled;
   };
