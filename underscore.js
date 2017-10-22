529e7098a47a03e80e0de57777b67991a66b58de
hongmaoxiao <769501294@qq.com>
2017-10-22 17:54:44 +0800

    use concat.apply for shallow flattening internally.

--- underscore.js
+++ underscore.js
@@ -28,6 +28,7 @@
   // Create quick reference variables for speed access to core prototypes.
   var push = ArrayProto.push,
     slice = ArrayProto.slice,
+    concat = ArrayProto.concat,
     unshift = ArrayProto.unshift,
     toString = ObjProto.toString,
     hasOwnProperty = ObjProto.hasOwnProperty;
@@ -499,7 +500,7 @@
   // Produce an array that contains the union: each distinct element from all of
   // the passed-in arrays.
   _.union = function() {
-    return _.uniq(_.flatten(arguments, true));
+    return _.uniq(concat.apply(ArrayProto, arguments));
   };
 
   // Produce an array that contains every item shared between two given arrays.
@@ -516,7 +517,7 @@
   // Take the difference between one array and another.
   // Only the elements present in just the first array will remain.
   _.difference = function(array) {
-    var rest = _.flatten(slice.call(arguments, 1), true);
+    var rest = concat.apply(ArrayProto, slice.call(arguments, 1));
     return _.filter(array, function(value) {
       return !_.include(rest, value);
     });
@@ -856,7 +857,7 @@
   // Restrict a given object to the properties named
   _.pick = function(obj) {
     var copy = {};
-    var keys = _.flatten(slice.call(arguments, 1));
+    var keys = concat.apply(ArrayProto, slice.call(arguments, 1));
     each(keys, function(key) {
       if (key in obj) {
         copy[key] = obj[key];
@@ -868,7 +869,7 @@
   // Return a copy of the object without the blacklisted properties.
   _.omit = function(obj) {
     var copy = {};
-    var keys = _.flatten(slice.call(arguments, 1));
+    var keys = concat.apply(ArrayProto, slice.call(arguments, 1));
     for (var key in obj) {
       if (!_.include(keys, key)) {
         copy[key] = obj[key];
