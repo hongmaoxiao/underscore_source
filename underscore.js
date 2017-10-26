dc8db504a09420f893d5387bfa68a1e5c2606dcd
hongmaoxiao <769501294@qq.com>
2017-10-27 07:34:25 +0800

    Ensure  triggers trailing call after repeatedly invoked. [closes #882]

--- underscore.js
+++ underscore.js
@@ -726,6 +726,7 @@
       args = arguments;
       if (remaining <= 0) {
         clearTimeout(timeout);
+        timeout = null;
         previous = now;
         result = func.apply(context, args);
       } else if (!timeout) {
